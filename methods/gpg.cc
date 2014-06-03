fad976e45be6b25942218a46a19a9cea46463c97
Tom Callaway <spot@fedoraproject.org>
2014-06-03 15:31:55 -0400

    fix format-security issue
    From: apt-0.5.15lorg3.95-format-security.patch

--- methods/gpg.cc
+++ methods/gpg.cc
@@ -369,7 +369,7 @@ bool GPGMethod::Fetch(FetchItem *Itm)
       if (msg != NULL) 
       {
 	 removeTmpDir(TempDir, SigCount);
-	 return _error->Error(msg);
+	 return _error->Error("%s", msg);
       }
    }
    else 
@@ -393,7 +393,7 @@ bool GPGMethod::Fetch(FetchItem *Itm)
 	 if (msg != NULL)
 	 {
 	    removeTmpDir(TempDir, SigCount);	       
-	    return _error->Error(msg);
+	    return _error->Error("%s", msg);
 	 }
 	 if (KeyList.empty())
 	    KeyList = KeyID;
